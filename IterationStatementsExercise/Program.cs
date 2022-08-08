8a0860c1c8cd367b4c58762f07c9e4bba88532bc
Joel <franco4prez@gmail.com>
2022-08-08 12:21:51 -0400

    first commit

---
+++ IterationStatementsExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationStatementsExercise
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i < 16; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
